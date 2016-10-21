97179e6888f9ca195586ed85026e815a2c15e64a
Jared J <peeepers@users.noreply.github.com>
2016-10-21 17:47:15 -0400

    Update milight-manager.groovy

--- smartapps/cloudsyjared/milight-manager.src/milight-manager.groovy
+++ smartapps/cloudsyjared/milight-manager.src/milight-manager.groovy
@@ -81,7 +81,7 @@ def buildColorPath(hex, evt) {
 
 def httpCall(path, mac, evt) {
     def params = [
-        uri:  'https://cloud.mithings.pw/v1/',
+        uri:  'https://api.mithings.pw/v1/',
         path: "$path",
         contentType: 'application/json',
         headers: [MAC:"$mac"]
